9bd566cf2345b39c11745483acd6b07952fd26f7
arinpr <arindam_pradhan@userexperiencedesignindia.com>
2026-08-31 18:56:47 +0530

    Updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     defaultConfig {
         applicationId = "com.soundshare.soundshare"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
